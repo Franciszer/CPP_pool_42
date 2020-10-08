265d19432488ef6476d6da63edc4f24ebf8eb07c
Franciszer <ftthierree@gmail.com>
2020-10-08 20:42:39 +0200

    module 08 finitozer

--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mutantstack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 16:40:33 by frthierr          #+#    #+#             */
-/*   Updated: 2020/10/08 18:14:29 by frthierr         ###   ########.fr       */
+/*   Updated: 2020/10/08 20:41:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,12 @@ class MutantStack: public std::stack<T> {
 		virtual ~MutantStack(void) {};
 
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		// container_type refers to the container embedded in the stack, which can be a dequeue, a vector, etc (see c++ doc for more information)
 		
-		using std::stack<T>::operator=;
+		// container_type refers to the container embedded in the stack, which can be a dequeue(default), a vector, etc (see c++ doc for more information)
+		MutantStack	&operator=(MutantStack &rhs) {
+			if (this != &rhs) *this = rhs;
+			return *this; 
+		}
 
 		// c is the container embedded in the stack
 		iterator begin(void) { return std::begin(std::stack<T>::c); }
