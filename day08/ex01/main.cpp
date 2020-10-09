69789de479b53c4f0491f3bf47983a1e5883dab7
Franciszer <ftthierree@gmail.com>
2020-10-09 11:51:17 +0200

    module 08 done and fixed

--- day08/ex01/main.cpp
+++ day08/ex01/main.cpp
@@ -65,3 +65,16 @@ int main()
 	 std::cout << "assignCopy longestSpan: " << assignCopy.longestSpan() << std::endl;
         std::cout << "opCopy longestSpan: " << opCopy.longestSpan() << std::endl;
 }
+
+
+// int main()
+// {
+//     Span sp = Span(5);
+// sp.addNumber(5);
+// sp.addNumber(3);
+// sp.addNumber(17);
+// sp.addNumber(9);
+// sp.addNumber(11);
+// std::cout << sp.shortestSpan() << std::endl;
+// std::cout << sp.longestSpan() << std::endl;
+// }
